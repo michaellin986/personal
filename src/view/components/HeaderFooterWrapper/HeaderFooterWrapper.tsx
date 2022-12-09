d49e8bc0174156f787764bc9689d7b6ce0f0e68a
Michael Lin <michaellin986@gmail.com>
2022-12-09 13:20:55 -0600

    Add wrapped component

--- src/view/components/HeaderFooterWrapper/HeaderFooterWrapper.tsx
+++ src/view/components/HeaderFooterWrapper/HeaderFooterWrapper.tsx
@@ -1,4 +1,6 @@
+import "./HeaderFooterWrapper.scss";
 import React, { FunctionComponent, PureComponent, ComponentType } from "react";
+import { NavLink } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,29 +12,67 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const pages = ["Home", "Work", "Projects", "Music"];
-export function withHeaderFooterWrapper(component: ComponentType) {
+
+export function withHeaderFooterWrapper(WrappedComponent: ComponentType) {
   return class HeaderFooterWrapper extends PureComponent {
     render() {
       return (
-        <AppBar position="static">
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                {pages.map((page) => (
-                  <Button
-                    key={page}
-                    // onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    {page}
-                  </Button>
-                ))}
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
+        <div>
+          <div className="HeaderFooterWrapper--header">
+            <div className="HeaderFooterWrapper--header__container">
+              <div className="HeaderFooterWrapper--header__name">
+                Michael Lin
+              </div>
+              <div className="HeaderFooterWrapper--header__buttons">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    "HeaderFooterWrapper--header__button" +
+                    (isActive
+                      ? " HeaderFooterWrapper--header__buttonActive"
+                      : "")
+                  }
+                  style={{ textDecoration: "none" }}
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/work"
+                  className={({ isActive }) =>
+                    "HeaderFooterWrapper--header__button" +
+                    (isActive
+                      ? " HeaderFooterWrapper--header__buttonActive"
+                      : "")
+                  }
+                  style={{ textDecoration: "none" }}
+                >
+                  Work
+                </NavLink>
+              </div>
+            </div>
+          </div>
+          <div className="HeaderFooterWrapper--wrappedComponent">
+            <WrappedComponent />
+          </div>
+          <div className="HeaderFooterWrapper--footer">
+            <div className="HeaderFooterWrapper--footer__social">
+              <LinkedInIcon className="HeaderFooterWrapper--footer__socialIcon" />
+              <GitHubIcon className="HeaderFooterWrapper--footer__socialIcon" />
+            </div>
+            <div className="HeaderFooterWrapper--footer__copyright">
+              <p>{"Created with React.js in Typescript"}</p>
+              <p>
+                {"Copyright © " + new Date().getFullYear() + " Michael Lin"}
+              </p>
+            </div>
+          </div>
+        </div>
       );
     }
   };

82416df28946a2d775582ce62e9e3ff0e5865caa
Michael Lin <michaellin986@gmail.com>
2022-12-09 13:29:20 -0600

    Remove unused libraries

--- src/view/components/HeaderFooterWrapper/HeaderFooterWrapper.tsx
+++ src/view/components/HeaderFooterWrapper/HeaderFooterWrapper.tsx
@@ -1,23 +1,8 @@
 import "./HeaderFooterWrapper.scss";
-import React, { FunctionComponent, PureComponent, ComponentType } from "react";
+import { PureComponent, ComponentType } from "react";
 import { NavLink } from "react-router-dom";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import MenuIcon from "@mui/icons-material/Menu";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
-
-const pages = ["Home", "Work", "Projects", "Music"];
 
 export function withHeaderFooterWrapper(WrappedComponent: ComponentType) {
   return class HeaderFooterWrapper extends PureComponent {
@@ -66,10 +51,7 @@ export function withHeaderFooterWrapper(WrappedComponent: ComponentType) {
               <GitHubIcon className="HeaderFooterWrapper--footer__socialIcon" />
             </div>
             <div className="HeaderFooterWrapper--footer__copyright">
-              <p>{"Created with React.js in Typescript"}</p>
-              <p>
-                {"Copyright © " + new Date().getFullYear() + " Michael Lin"}
-              </p>
+              {"Copyright © " + new Date().getFullYear() + " Michael Lin"}
             </div>
           </div>
         </div>

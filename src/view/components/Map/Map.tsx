0c0633fa5b2353543b1b496cfd00c98472838637
Michael Lin <michaellin986@gmail.com>
2022-12-12 13:01:04 -0600

    Refactor

--- src/view/components/Map/Map.tsx
+++ src/view/components/Map/Map.tsx
@@ -1,15 +1,17 @@
+import variables from "../../../main.scss";
 import { PureComponent } from "react";
 import data from "../../../assets/data/Travel.json";
 
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_worldGreatLakesHigh from "@amcharts/amcharts5-geodata/worldGreatLakesHigh";
+import am5geodata_usaHigh from "@amcharts/amcharts5-geodata/usaHigh";
 
 const routes: string[][] = data.routes;
 const coordinates: {
   [key: string]: number[];
 } = data.coordinates;
-const countries: string[] = data.countries;
+const regions: string[] = data.regions;
 
 const points = Object.keys(coordinates).map((loc) => ({
   geometry: {
@@ -40,6 +42,66 @@ class Map extends PureComponent {
     this.root = undefined;
   }
 
+  createPolygonSeries(root: am5.Root, chart: am5map.MapChart): void {
+    const polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_worldGreatLakesHigh,
+        exclude: ["AQ"],
+        fill: am5.color(variables.colorSecondary),
+      })
+    );
+    polygonSeries.mapPolygons.template.setAll({
+      templateField: "polygonSettings",
+    });
+    polygonSeries.data.setAll(
+      regions.map((region) => {
+        return {
+          id: region,
+          polygonSettings: {
+            fill: am5.color(variables.colorTertiary),
+          },
+        };
+      })
+    );
+    chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_usaHigh,
+        fill: am5.color(variables.colorTertiary),
+      })
+    );
+  }
+
+  createPointSeries(root: am5.Root, chart: am5map.MapChart): void {
+    const pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
+    pointSeries.bullets.push(() => {
+      return am5.Bullet.new(root, {
+        sprite: am5.Circle.new(root, {
+          radius: 3,
+          fill: am5.color(variables.colorQuaternary),
+          stroke: am5.color(variables.colorPrimary),
+          tooltipText: "{label}",
+          tooltipPosition: "pointer",
+        }),
+      });
+    });
+    pointSeries.data.setAll(points);
+  }
+
+  createLineSeries(root: am5.Root, chart: am5map.MapChart): void {
+    const lineSeries = chart.series.push(
+      am5map.MapLineSeries.new(root, {
+        stroke: am5.color(variables.colorPrimary),
+        lineType: "curved",
+      })
+    );
+    lineSeries.mapLines.template.setAll({
+      strokeWidth: 1.5,
+      tooltipText: "{label}",
+      tooltipPosition: "pointer",
+    });
+    lineSeries.data.setAll(lines);
+  }
+
   componentDidMount(): void {
     const root = am5.Root.new("chartdiv");
     const chart = root.container.children.push(
@@ -51,55 +113,9 @@ class Map extends PureComponent {
       })
     );
 
-    const polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: am5geodata_worldGreatLakesHigh,
-        exclude: ["AQ"],
-        fill: am5.color("#61dbfb"),
-      })
-    );
-
-    polygonSeries.mapPolygons.template.setAll({
-      templateField: "polygonSettings",
-    });
-
-    polygonSeries.data.setAll(
-      countries.map((country) => {
-        return {
-          id: country,
-          polygonSettings: {
-            fill: am5.color("#4169e1"),
-          },
-        };
-      })
-    );
-
-    const pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
-    pointSeries.bullets.push(() => {
-      return am5.Bullet.new(root, {
-        sprite: am5.Circle.new(root, {
-          radius: 3,
-          fill: am5.color("#ff0000"),
-          stroke: am5.color("#ffffff"),
-          tooltipText: "{label}",
-          tooltipPosition: "pointer",
-        }),
-      });
-    });
-    pointSeries.data.setAll(points);
-    const lineSeries = chart.series.push(
-      am5map.MapLineSeries.new(root, {
-        stroke: am5.color("#ffffff"),
-        lineType: "curved",
-      })
-    );
-    lineSeries.mapLines.template.setAll({
-      strokeWidth: 1.5,
-      tooltipText: "{label}",
-      tooltipPosition: "pointer",
-    });
-    lineSeries.data.setAll(lines);
-
+    this.createPolygonSeries(root, chart);
+    this.createPointSeries(root, chart);
+    this.createLineSeries(root, chart);
     this.root = root;
   }
 
